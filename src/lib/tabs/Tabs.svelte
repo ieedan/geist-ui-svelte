f3e166a4797b4ab267c90aee0b7de433371fc9cb
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-05 15:10:35 -0600

    Moved CSS
    - Moved some CSS to be self contained
    - Added CSS recommendation to README

--- src/lib/tabs/Tabs.svelte
+++ src/lib/tabs/Tabs.svelte
@@ -40,21 +40,33 @@
 
 <div
 	class="flex place-items-center border-b px-2 border-gray-100 overflow-hidden w-full
-dark:border-gray-900 relative"
->
+dark:border-gray-900 relative">
 	<!-- svelte-ignore a11y-mouse-events-have-key-events -->
 	<nav
 		bind:this={elementRef}
 		class="flex place-items-center transition-all overflow-x-auto scrollbar-hide"
 		on:mouseover={hover}
 		on:scroll={scroll}
-		on:mouseleave={() => (showHoverBackground = false)}
-	>
+		on:mouseleave={() => (showHoverBackground = false)}>
 		<slot />
 	</nav>
 	<div
 		bind:this={hoverBackgroundRef}
 		class="absolute z-[0] rounded-md bg-gray-100 transition-all data-[show=false]:opacity-0 dark:bg-gray-900"
-		data-show={showHoverBackground}
-	></div>
+		data-show={showHoverBackground}></div>
 </div>
+
+<style lang="postcss">
+	.scrollbar-hide {
+		-ms-overflow-style: none; /* Internet Explorer and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+
+	.scrollbar-hide::-webkit-scrollbar {
+		display: none; /* Chrome, Safari, and Opera */
+	}
+
+	::-webkit-scrollbar-corner {
+		background: rgba(0, 0, 0, 0);
+	}
+</style>
