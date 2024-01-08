7c0973a66d73b8a5a34303435116b92407d2d722
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-07 20:28:52 -0600

    Styling
    - Still havent fixed issue with scrolling
    - also need to lock scroll

--- src/lib/drawer/Drawer.svelte
+++ src/lib/drawer/Drawer.svelte
@@ -25,13 +25,13 @@
 <div
 	data-show={visible}
 	data-placement={placement}
-	class="absolute data-[show=false]:pointer-events-none data-[show=false]:opacity-0 z-50
+	class="fixed data-[show=false]:pointer-events-none data-[show=false]:opacity-0 z-50
     data-[show=false]:data-[placement='left']:-translate-x-2 data-[show=false]:data-[placement='right']:translate-x-2
     data-[show=false]:data-[placement='top']:-translate-y-2 data-[show=false]:data-[placement='bottom']:translate-y-2
     transition-all data-[placement='top']:w-full data-[placement='top']:h-1/4 data-[placement='bottom']:w-full 
     data-[placement='bottom']:h-1/4 w-1/4 bg-gray-0 dark:bg-gray-999 h-svh data-[placement='bottom']:bottom-0
     data-[placement='bottom']:left-0 data-[placement='top']:left-0 data-[placement='top']:top-0 
-    data-[placement='left']:left-0 data-[placement='left']:top-0
+    data-[placement='left']:left-0 data-[placement='left']:top-0 border-gray-100 dark:border-gray-900
     data-[placement='right']:right-0 data-[placement='right']:top-0
     data-[placement='left']:border-r data-[placement='right']:border-l
     data-[placement='bottom']:border-t data-[placement='top']:border-b">
