0c1ec95d6e74e5cff84e494462c3c2ac227de7a4
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-09 18:08:03 -0600

    working on hero docs
    - Completed fieldset docs

--- src/routes/components/fieldset/+page.svelte
+++ src/routes/components/fieldset/+page.svelte
@@ -1,5 +1,61 @@
-<script>
-	import Note from "$lib/note/Note.svelte";
+<script lang="ts">
+	import Button from "$lib/button/Button.svelte";
+	import Code from "$lib/code/Code.svelte";
+	import Details from "$lib/details/Details.svelte";
+	import FieldSet from "$lib/fieldset/FieldSet.svelte";
+	import Input from "$lib/input/Input.svelte";
+	import Snippet from "$lib/snippet/Snippet.svelte";
+	import Spacer from "$lib/spacer/Spacer.svelte";
+	import Text from "$lib/text/Text.svelte";
 </script>
 
-<Note color="warning">These docs are under construction you should see something here soon!</Note>
+<Text type="h3">Fieldset</Text>
+<Spacer h={10} />
+<Text>An organizational layout component to group related information.</Text>
+<Spacer h={20} />
+<Snippet width="450px" type="lite" text={`import { Fieldset } from 'geist-ui-svelte';`} />
+<Spacer h={30} />
+<Text type="h4">Basic</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start">
+		<FieldSet>
+			<div class="py-2">
+				<Text type="h4">Team Name</Text>
+				<Spacer h={5} />
+				<Text class="text-sm">
+					This is your team's visible name within geist-ui-svelte. For example, the name
+					of your company or department.
+				</Text>
+				<Spacer h={10} />
+				<Input value="Aidan's Team Name" />
+			</div>
+			<div slot="footer" class="flex justify-between place-items-center">
+				<Text type="small">Please use 32 characters at maximum.</Text>
+				<Button color="secondary-light" size="sm">Save</Button>
+			</div>
+		</FieldSet>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<FieldSet>
+	<div class="py-2">
+		<Text type="h4">Team Name</Text>
+		<Spacer h={5} />
+		<Text class="text-sm">
+			This is your team's visible name within geist-ui-svelte. For example, the name
+			of your company or department.
+		</Text>
+		<Spacer h={10} />
+		<Input value="Aidan's Team Name" />
+	</div>
+	<div slot="footer" class="flex justify-between place-items-center">
+		<Text type="small">Please use 32 characters at maximum.</Text>
+		<Button color="secondary-light" size="sm">Save</Button>
+	</div>
+</FieldSet>`} />
+		</Details>
+	</div>
+</FieldSet>
--- src/routes/components/hero/+page.svelte
+++ src/routes/components/hero/+page.svelte
@@ -1,5 +1,57 @@
-<script>
-	import Note from "$lib/note/Note.svelte";
+<script lang="ts">
+	import Code from "$lib/code/Code.svelte";
+	import Details from "$lib/details/Details.svelte";
+	import FieldSet from "$lib/fieldset/FieldSet.svelte";
+	import Snippet from "$lib/snippet/Snippet.svelte";
+	import Spacer from "$lib/spacer/Spacer.svelte";
+	import Text from "$lib/text/Text.svelte";
+	import Page from '$lib/page/Page.svelte'
+	import Button from "$lib/button/Button.svelte";
+	import { Hero } from "$lib/index.js";
+
+	export let pageVisible = false;
 </script>
 
-<Note color="warning">These docs are under construction you should see something here soon!</Note>
+<Text type="h3">Hero</Text>
+<Spacer h={10} />
+<Text>A layout component to help you create your hero pages.</Text>
+<Spacer h={20} />
+<Snippet width="450px" type="lite" text={`import { Hero } from 'geist-ui-svelte';`} />
+<Spacer h={30} />
+<Text type="h4">Basic</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start">
+		<Button on:click={() => pageVisible = true}>Show Example</Button>
+		<Page bind:visible={pageVisible}>
+			<Hero>
+				<Text type="h1">geist-ui-svelte</Text>
+			</Hero>
+			<Hero type="secondary">
+				
+			</Hero>
+		</Page>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<FieldSet>
+	<div class="py-2">
+		<Text type="h4">Team Name</Text>
+		<Spacer h={5} />
+		<Text class="text-sm">
+			This is your team's visible name within geist-ui-svelte. For example, the name
+			of your company or department.
+		</Text>
+		<Spacer h={10} />
+		<Input value="Aidan's Team Name" />
+	</div>
+	<div slot="footer" class="flex justify-between place-items-center">
+		<Text type="small">Please use 32 characters at maximum.</Text>
+		<Button color="secondary-light" size="sm">Save</Button>
+	</div>
+</FieldSet>`} />
+		</Details>
+	</div>
+</FieldSet>
