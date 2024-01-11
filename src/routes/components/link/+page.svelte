25d461dbb0195c2ae71e568bae7436fd1aacf845
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-11 15:17:43 -0600

    Completed link docs

--- src/routes/components/link/+page.svelte
+++ src/routes/components/link/+page.svelte
@@ -1,5 +1,90 @@
-<script>
-	import Note from "$lib/note/Note.svelte";
+<script lang="ts">
+	import Code from "$lib/code/Code.svelte";
+	import Details from "$lib/details/Details.svelte";
+	import FieldSet from "$lib/fieldset/FieldSet.svelte";
+	import Snippet from "$lib/snippet/Snippet.svelte";
+	import Spacer from "$lib/spacer/Spacer.svelte";
+	import Text from "$lib/text/Text.svelte";
+	import Link from "$lib/link/Link.svelte";
 </script>
 
-<Note color="warning">These docs are under construction you should see something here soon!</Note>
+<Text type="h3">Link</Text>
+<Spacer h={10} />
+<Text>A simple link component.</Text>
+<Spacer h={20} />
+<Snippet width="450px" type="lite" text={`import { Link } from 'geist-ui-svelte';`} />
+<Spacer h={30} />
+<Text type="h4">Basic</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start">
+		<Link to="https://github.com/ieedan/geist-ui-svelte" target="_blank">
+			Check out the geist-ui-svelte here
+		</Link>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<Link
+	to="https://github.com/ieedan/geist-ui-svelte" 
+	target="_blank">
+	Check out the geist-ui-svelte here
+</Link>`} />
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">Highlight</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start">
+		<Link to="https://github.com/ieedan/geist-ui-svelte" target="_blank" color>
+			Check out the geist-ui-svelte here
+		</Link>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<Link 
+	to="https://github.com/ieedan/geist-ui-svelte" 
+	target="_blank" 
+	color>
+	Check out the geist-ui-svelte here
+</Link>`} />
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">Underline</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start">
+		<Link to="https://github.com/ieedan/geist-ui-svelte" target="_blank" underline>
+			Check out the geist-ui-svelte here
+		</Link>
+		<Link to="https://github.com/ieedan/geist-ui-svelte" target="_blank" color underline>
+			Check out the geist-ui-svelte here
+		</Link>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<Link 
+	to="https://github.com/ieedan/geist-ui-svelte" 
+	target="_blank" 
+	underline>
+	Check out the geist-ui-svelte here
+</Link>
+<Link 
+	to="https://github.com/ieedan/geist-ui-svelte"
+	target="_blank" 
+	color 
+	underline>
+	Check out the geist-ui-svelte here
+</Link>`} />
+		</Details>
+	</div>
+</FieldSet>
