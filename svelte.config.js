c30f70dbf6e8968369b642f052d55addf7294c77
SikandarJODD <vu1f2021072@pvppcoe.ac.in>
2024-01-11 19:26:27 +0530

    Updated config

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
+	// Check the branch
 	preprocess: vitePreprocess(),
 
 	kit: {
