8dd76657741e06b8a59a7f14fd43745fd75ff312
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-11 15:00:07 -0600

    Added dotcumentation

--- src/lib/dot/Dot.svelte
+++ src/lib/dot/Dot.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	export let type: "secondary" | "success" | "warning" | "error" | "go" = "secondary";
+	export let type: "default" | "secondary" | "success" | "warning" | "error" | "go" = "default";
 	export let size: number = 12;
 	export let style: string = "";
+	export let color: string | undefined = undefined;
 </script>
 
 <span
 	{...$$restProps}
-	style="width: {size}px; height: {size}px;{style}"
+	style="width: {size}px; height: {size}px; {color ? `background-color: ${color};` : ""}{style}"
 	data-type={type}
-	class="rounded-full bg-gray-200 data-[type='success']:bg-blue-500
+	class="rounded-full bg-gray-999 dark:bg-gray-0 data-[type='secondary']:bg-gray-200 data-[type='success']:bg-blue-500
   data-[type='warning']:bg-orange-300 data-[type='go']:bg-green-600 data-[type='go']:dark:bg-green-700
   data-[type='warning']:dark:bg-orange-400 data-[type='error']:bg-red-500
     data-[type='error']:dark:bg-red-600 inline-block"
--- src/routes/components/dot/+page.svelte
+++ src/routes/components/dot/+page.svelte
@@ -1,5 +1,79 @@
-<script>
-	import Note from "$lib/note/Note.svelte";
+<script lang="ts">
+	import Code from "$lib/code/Code.svelte";
+	import Details from "$lib/details/Details.svelte";
+	import Dot from "$lib/dot/Dot.svelte";
+	import FieldSet from "$lib/fieldset/FieldSet.svelte";
+	import Snippet from "$lib/snippet/Snippet.svelte";
+	import Spacer from "$lib/spacer/Spacer.svelte";
+	import Text from "$lib/text/Text.svelte";
 </script>
 
-<Note color="warning">These docs are under construction you should see something here soon!</Note>
+<Text type="h3">Dot</Text>
+<Spacer h={10} />
+<Text>A circle component great as a status indicator.</Text>
+<Spacer h={20} />
+<Snippet width="400px" type="lite" text={`import { Dot } from 'geist-ui-svelte';`} />
+<Spacer h={30} />
+<Text type="h4">Sizes</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex gap-2 justify-start place-items-end">
+		<Dot/>
+		<Dot size={16}/>
+		<Dot size={20}/>
+		<Dot size={24}/>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<Dot/>
+<Dot size={16}/>
+<Dot size={20}/>
+<Dot size={24}/>`}
+			/>
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">Colors</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex gap-2 justify-start place-items-end">
+		<Dot/>
+		<Dot type="success"/>
+		<Dot type="warning"/>
+		<Dot type="error"/>
+		<Dot type="secondary"/>
+		<Dot type="go"/>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<Dot/>
+<Dot type="success"/>
+<Dot type="warning"/>
+<Dot type="error"/>
+<Dot type="secondary"/>
+<Dot type="go"/>`}
+			/>
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">Custom Color</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex gap-2 justify-start place-items-end">
+		<Dot color="#ff00ff"/>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<Dot color="#ff00ff"/>`}
+			/>
+		</Details>
+	</div>
+</FieldSet>
