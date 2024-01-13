664bf0a105d22345d03a9f2b429d78dd3aa02c2f
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-13 11:23:26 -0600

    Added optional animate attribute

--- src/lib/table/Table.svelte
+++ src/lib/table/Table.svelte
@@ -1,35 +1,40 @@
 <script lang="ts">
-    type Column = {
-        label: string;
-        property: string;
-    }
-    
-    type DataItem = {
-        [key: string]: string;
-    }
+	import type { Column, DataItem } from '$lib/types.js';
+    import { fly } from 'svelte/transition';
 
-    export let columns: Column[];
-    export let data: DataItem[];
+	export let columns: Column[];
+	export let data: DataItem[];
+    export let animate = false;
+    $: duration = animate ? 250 : 0;
 </script>
 
-<div role="table">
-    <div class="border border-gray-100 dark:border-gray-900 py-3 rounded-md px-2 dark:bg-gray-950 bg-gray-50">
-        <div role="row" class="flex place-items-center">
-            {#each columns as column}
-                <div class="w-full text-gray-600 dark:text-gray-600 font-light text-xs">{column.label.toUpperCase()}</div>
-            {/each}
-        </div>
-    </div>
-    <div >
+<div role="table" class="w-full">
+	<div
+		class="border border-gray-100 dark:border-gray-900 
+        py-3 rounded-md px-2 dark:bg-gray-950 bg-gray-50">
+		<div role="row" class="flex place-items-center">
+			{#each columns as column}
+				<div class="w-full text-gray-600 dark:text-gray-600 font-light text-xs">
+                    {column.label.toUpperCase()}
+                </div>
+			{/each}
+		</div>
+	</div>
+	<div>
+		<!-- need to find a way to do keying in a user friendly way -->
 
-        <!-- need to find a way to do keying in a user friendly way -->
-
-        {#each data as row}
-            <div role="row" class="flex place-items-center px-2 py-2 border-b border-gray-100 dark:border-gray-900">
-                {#each columns as column}
-                    <div class="w-full text-gray-600 dark:text-gray-600">{row[column.property]}</div>
-                {/each}
-            </div>
-        {/each}
-    </div>
+		{#each data as row}
+			<div transition:fly={{ y: 2, duration: duration }}
+				role="row"
+				class="flex place-items-center px-2 py-2 border-b 
+                border-gray-100 dark:border-gray-900 hover:bg-gray-50 
+                hover:dark:bg-gray-950 transition-all">
+				{#each columns as column}
+					<div class="w-full text-gray-600 dark:text-gray-600">
+                        {row[column.property]}
+                    </div>
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
--- src/routes/components/table/+page.svelte
+++ src/routes/components/table/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from "$lib/button/Button.svelte";
 	import Code from "$lib/code/Code.svelte";
 	import Details from "$lib/details/Details.svelte";
 	import FieldSet from "$lib/fieldset/FieldSet.svelte";
@@ -7,18 +8,68 @@
 	import Table from "$lib/table/Table.svelte";
 	import Text from "$lib/text/Text.svelte";
 
+    /* eslint-disable no-useless-escape */
+    const firstExampleCode = `<script lang="ts">
+    import Button from "$lib/button/Button.svelte";
+    import Table from "$lib/table/Table.svelte";
+
+    let data = [
+		{
+			property: "\`edits\`",
+			description: "used to highlight sections of code",
+			type: "Edit[]",
+		},
+		{
+			property: "\`lineNumber\`",
+			description: "enable / disable line numbers",
+			type: "boolean",
+		},
+	];
+
+	const addData = () => {
+		data = [
+			...data,
+			{
+				property: "\`lineNumber\`",
+				description: "enable / disable line numbers",
+				type: "boolean",
+			},
+		];
+	};
+<\/script>
+
+<Button on:click={addData}>Add row</Button>
+<Table
+	bind:data
+	columns={[
+		{ label: "Property", property: "property" },
+		{ label: "Description", property: "description" },
+	    { label: "Type", property: "type" },
+    ]} />`;
+
 	let data = [
 		{
 			property: "`edits`",
-            description: "used to highlight sections of code",
-            type: "Edit[]"
+			description: "used to highlight sections of code",
+			type: "Edit[]",
 		},
-        {
+		{
 			property: "`lineNumber`",
-            description: "enable / disable line numbers",
-            type: "boolean"
+			description: "enable / disable line numbers",
+			type: "boolean",
 		},
 	];
+
+	const addData = () => {
+		data = [
+			...data,
+			{
+				property: "`lineNumber`",
+				description: "enable / disable line numbers",
+				type: "boolean",
+			},
+		];
+	};
 </script>
 
 <Text type="h3">Table</Text>
@@ -30,9 +81,10 @@
 <Text type="h4">Basic</Text>
 <Spacer h={10} />
 <FieldSet>
-	<div class="flex flex-col justify-start">
+	<div class="flex flex-col justify-center place-items-center gap-2">
+        <Button on:click={addData}>Add row</Button>
 		<Table
-            bind:data={data}
+			bind:data
 			columns={[
 				{ label: "Property", property: "property" },
 				{ label: "Description", property: "description" },
@@ -41,12 +93,9 @@
 	</div>
 	<div slot="footer">
 		<Details label="Code">
-			<Code lang="svelte" code={`<Table
-    bind:data={data}
-    columns={[
-        { label: "Property", property: "property" },
-        { label: "Description", property: "description" },
-    ]} />`} />
+			<Code
+				lang="svelte"
+				code={firstExampleCode} />
 		</Details>
 	</div>
 </FieldSet>
