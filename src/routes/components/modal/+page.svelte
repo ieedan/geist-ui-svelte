b49f6ba58282f1d864b9be7fd6f5bc58025984b4
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-12 20:49:55 -0600

    Completed modal docs

--- src/routes/components/modal/+page.svelte
+++ src/routes/components/modal/+page.svelte
@@ -7,6 +7,7 @@
 	import Text from "$lib/text/Text.svelte";
 	import Modal from "$lib/modal/Modal.svelte";
 	import Button from "$lib/button/Button.svelte";
+	import { Edit } from "$lib/types.js";
 
 	export let showFirstExample = false;
 </script>
@@ -18,11 +19,16 @@
 <Snippet width="450px" type="lite" text={`import { Modal } from 'geist-ui-svelte';`} />
 <Spacer h={30} />
 <Text type="h4">Basic</Text>
+<Spacer h={5} />
+<Text>
+	Use <code>`sm:h-[]`</code> / <code>`sm:w-[]`</code> to customize the height and width of the modal
+	when the screen width is greater than 640px.
+</Text>
 <Spacer h={10} />
 <FieldSet>
 	<div class="flex flex-col justify-start">
 		<Button on:click={() => (showFirstExample = true)}>Show modal</Button>
-		<Modal bind:visible={showFirstExample} class="w-[640px]">
+		<Modal bind:visible={showFirstExample} class="sm:w-[640px]">
 			<div class="p-4 flex flex-col place-items-center justify-center h-full">
 				<Text align="center">
 					This is a modal notice how it goes to the bottom of the screen on a smaller
@@ -39,22 +45,26 @@
 		<Details label="Code">
 			<Code
 				lang="svelte"
-				code={`<Button 
-	on:click={() => (showFirstExample = true)}>
+				edits={[{ lineNumber: 6, type: Edit.add }]}
+				code={`<Button on:click={() => (showFirstExample = true)}>
 	Show modal
 </Button>
-<Modal bind:visible={showFirstExample} class="w-[640px]">
+<Modal 
+	bind:visible={showFirstExample}
+ 	class="sm:w-[640px]">
 	<div 
-	class="p-4 flex flex-col place-items-center 
-	justify-center h-full">
+	class="p-4 flex flex-col place-items-center
+	 justify-center h-full">
 		<Text align="center">
-			This is a modal not...
+			This is a modal notice how it goes 
+			to the bottom of the screen on a smaller
+			view.
 		</Text>
-		<Spacer h={30}/>
+		<Spacer h={30} />
 		<Text 
-		align="center" color="secondary">
-			Click outside the modal or press 
-			<code>\`Esc\`</code>.
+	align="center" color="secondary">
+		Click outside the modal or press 
+		<code>\`Esc\`</code>.
 		</Text>
 	</div>
 </Modal>`} />
