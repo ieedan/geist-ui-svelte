5e98e0f978647718693518a9f984f844d8aaa03a
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-15 14:51:14 -0600

    Added optional `Ring` to checkbox component
    - This is enabled with the `ring` attribute

--- src/lib/checkbox/Checkbox.svelte
+++ src/lib/checkbox/Checkbox.svelte
@@ -9,6 +9,7 @@
 	export let id: string | undefined = undefined;
 	export let color: Color = "default";
 	export let disabled: boolean = false;
+	export let ring: boolean = false;
 
 	const toggle = () => {
 		checked = !checked;
@@ -20,8 +21,7 @@
 <label
 	for={id}
 	class="flex place-items-center gap-2 group aria-disabled:hover:cursor-not-allowed select-none hover:cursor-pointer"
-	aria-disabled={disabled}
->
+	aria-disabled={disabled}>
 	<button
 		on:click|preventDefault|stopPropagation={toggle}
 		{id}
@@ -30,7 +30,7 @@
 		data-color={color}
 		{disabled}
 		class="flex place-items-center justify-center border border-gray-200 dark:border-gray-800 rounded-md size-4 aria-checked:border-gray-999
-        dark:aria-checked:border-gray-0 data-[color='success']:aria-checked:border-blue-500
+        dark:aria-checked:border-gray-0 data-[color='success']:aria-checked:border-blue-500 outline-none focus:outline-none
         data-[color='warning']:aria-checked:border-orange-300 data-[color='secondary']:aria-checked:border-gray-200
         data-[color='secondary']:dark:aria-checked:border-gray-800 data-[ghost=true]:dark:aria-checked:border-gray-999
         data-[ghost=true]:aria-checked:border-gray-0 data-[color='warning']:dark:aria-checked:border-orange-400
@@ -41,10 +41,18 @@
         data-[color='warning']:dark:aria-checked:bg-orange-400 data-[color='error']:aria-checked:bg-red-500
         data-[color='error']:dark:aria-checked:bg-red-600 data-[color='error']:aria-checked:border-red-500
 		disabled:bg-gray-50 disabled:hover:cursor-not-allowed disabled:aria-checked:data-[color]:bg-gray-300
-		disabled:aria-checked:data-[color]:border-gray-300 disabled:dark:bg-gray-950 .
+		disabled:aria-checked:data-[color]:border-gray-300 disabled:dark:bg-gray-950 group
 		disabled:dark:aria-checked:data-[color]:bg-gray-700 disabled:dark:aria-checked:data-[color]:border-gray-700
-		text-gray-0 dark:text-gray-999"
-	>
+		text-gray-0 dark:text-gray-999 transition-all relative">
+		{#if ring}
+			<div
+				class="absolute size-6 rounded-lg border-2 left-1/2 top-1/2 transition-all
+			-translate-y-1/2 -translate-x-1/2 group-focus:opacity-100 opacity-0
+			group-data-[color='success']:border-blue-600 dark:border-gray-0 border-gray-999
+			group-data-[color='warning']:border-orange-300 dark:group-data-[color='warning']:border-orange-400
+			group-data-[color='error']:border-red-500 dark:group-data-[color='error']:border-red-600
+			group-data-[color='secondary']:border-gray-200 dark:group-data-[color='secondary']:border-gray-800"></div>
+		{/if}
 		{#if checked}
 			<CheckMarkIcon size={16} />
 		{/if}
