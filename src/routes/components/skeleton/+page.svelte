03c4dc8b1dd9c736a2002e27563fda76983079f0
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-16 09:15:02 -0600

    Added second example that uses promise
    - Added new example and code making use of await block

--- src/routes/components/skeleton/+page.svelte
+++ src/routes/components/skeleton/+page.svelte
@@ -7,6 +7,63 @@
 	import Text from "$lib/text/Text.svelte";
 	import Skeleton from "$lib/skeleton/Skeleton.svelte";
 	import SkeletonContainer from "$lib/skeleton/SkeletonContainer.svelte";
+	import Button from "$lib/button/Button.svelte";
+	import User from "$lib/user/User.svelte";
+	import { onMount } from "svelte";
+
+	let loading = true;
+	let promise: Promise<boolean>;
+
+	const createPromise = () => {
+		loading = true;
+		promise = new Promise<boolean>((res) => {
+			setTimeout(() => res(true), 3000);
+		});
+		promise.then(() => loading = false);
+	};
+
+	onMount(() => {
+		createPromise();
+	});
+
+	/* eslint-disable no-useless-escape */
+	const secondExampleCode = `<script lang="ts">
+	let loading = true;
+	let promise: Promise<boolean>;
+
+	const createPromise = () => {
+		loading = true;
+		promise = new Promise<boolean>((res) => {
+			setTimeout(() => res(true), 3000);
+		});
+		promise.then(() => loading = false);
+	};
+
+	onMount(() => {
+		createPromise();
+	});
+<\/script>
+
+<Button
+	on:click={createPromise}
+	bind:loading>
+	Refresh
+</Button>
+{#await promise}
+	<SkeletonContainer 
+	loading={true} 
+	class="flex place-items-center gap-2">
+		<Skeleton class="size-10 rounded-full" />
+		<div class="flex flex-col gap-2">
+			<Skeleton class="w-14 h-4 rounded-md" />
+			<Skeleton class="w-24 h-4 rounded-md" />
+		</div>
+	</SkeletonContainer>
+{:then}
+	<div class="flex place-items-center gap-2">
+		<User name="Aidan" detail details="Trash dev" />
+	</div>
+{/await}`;
 </script>
 
 <Text type="h3">Skeleton</Text>
@@ -16,8 +73,7 @@
 <Snippet
 	width="600px"
 	type="lite"
-	text={`import { Skeleton, SkeletonContainer } from 'geist-ui-svelte';`}
-/>
+	text={`import { Skeleton, SkeletonContainer } from 'geist-ui-svelte';`} />
 <Spacer h={30} />
 <Text type="h4">Basic</Text>
 <Spacer h={5} />
@@ -48,8 +104,41 @@
         <Skeleton class="w-14 h-4 rounded-md" />
         <Skeleton class="w-24 h-4 rounded-md" />
     </div>
-</SkeletonContainer>`}
-			/>
+</SkeletonContainer>`} />
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">With promise</Text>
+<Spacer h={5} />
+<Text>Use a promise to show / hide skeleton content.</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start gap-2">
+		<Button
+			on:click={createPromise}
+			bind:loading>
+			Refresh
+		</Button>
+		{#await promise}
+			<SkeletonContainer loading={true} class="flex place-items-center gap-2">
+				<Skeleton class="size-10 rounded-full" />
+				<div class="flex flex-col gap-2">
+					<Skeleton class="w-14 h-4 rounded-md" />
+					<Skeleton class="w-24 h-4 rounded-md" />
+				</div>
+			</SkeletonContainer>
+		{:then}
+			<div class="flex place-items-center gap-2">
+				<User name="Aidan" detail details="Trash dev" />
+			</div>
+		{/await}
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={secondExampleCode} />
 		</Details>
 	</div>
 </FieldSet>
