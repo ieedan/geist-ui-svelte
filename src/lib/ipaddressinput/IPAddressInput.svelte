24ed0f341a8aa1781f0c851766f4d0f918a3a180
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-24 18:07:01 -0600

    Working on UI component

--- src/lib/ipaddressinput/IPAddressInput.svelte
+++ src/lib/ipaddressinput/IPAddressInput.svelte
@@ -2,14 +2,58 @@
 	import { IPAddress, type IPV4Address, type Octets } from "$lib/util/ip-address.js";
 	import { onMount } from "svelte";
 
-    /** '.' or ' ' separated IP address ex: '172.16.100.10' or '172 16 100 10' */
-    export let value: IPV4Address = "192 168 010 501";
+	/** '.' or ' ' separated IP address ex: '172.16.100.10' or '172 16 100 10' */
+	export let value: IPV4Address = "192 168 010 501";
+	export let noDot: boolean = false;
 
-    let octets: Octets = IPAddress.parseIPV4(value);
+	let octets: Octets = IPAddress.parseIPV4(value);
 
-    onMount(() => {
-        console.log(octets);
-        
-    })
+	onMount(() => {
+		console.log(octets);
+	});
 </script>
 
+<div
+	class="flex place-items-center border border-gray-100 dark:border-gray-900 w-fit rounded-md py-1 px-1">
+	<input
+		min="0"
+		max="255"
+		type="number"
+		bind:value={octets[0]}
+		class="min-w-0 bg-transparent w-10 hide-spinner outline-none text-center selection:bg-gray-999 selection:text-gray-0 selection:dark:bg-gray-0 selection:dark:text-gray-999" />
+	{#if !noDot}
+		<span>.</span>
+	{/if}
+	<input
+		min="0"
+		max="255"
+		type="number"
+		bind:value={octets[1]}
+		class="min-w-0 bg-transparent w-10 hide-spinner outline-none text-center selection:bg-gray-999 selection:text-gray-0 selection:dark:bg-gray-0 selection:dark:text-gray-999" />
+	{#if !noDot}
+		<span>.</span>
+	{/if}
+	<input
+		min="0"
+		max="255"
+		type="number"
+		bind:value={octets[2]}
+		class="min-w-0 bg-transparent w-10 hide-spinner outline-none text-center selection:bg-gray-999 selection:text-gray-0 selection:dark:bg-gray-0 selection:dark:text-gray-999" />
+	{#if !noDot}
+		<span>.</span>
+	{/if}
+	<input
+		min="0"
+		max="255"
+		type="number"
+		bind:value={octets[3]}
+		class="min-w-0 bg-transparent w-10 hide-spinner outline-none text-center selection:bg-gray-999 selection:text-gray-0 selection:dark:bg-gray-0 selection:dark:text-gray-999" />
+</div>
+
+<style lang="postcss">
+	.hide-spinner::-webkit-inner-spin-button,
+	.hide-spinner::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+</style>
--- src/routes/components/ipaddressinput/+page.svelte
+++ src/routes/components/ipaddressinput/+page.svelte
@@ -2,33 +2,47 @@
 	import Code from "$lib/code/Code.svelte";
 	import Details from "$lib/details/Details.svelte";
 	import FieldSet from "$lib/fieldset/FieldSet.svelte";
-	import IpAddressInput from "$lib/ipaddressinput/IPAddressInput.svelte";
+	import IPAddressInput from "$lib/ipaddressinput/IPAddressInput.svelte";
 	import Snippet from "$lib/snippet/Snippet.svelte";
 	import Spacer from "$lib/spacer/Spacer.svelte";
 	import Text from "$lib/text/Text.svelte";
 </script>
 
-<Text type="h3">Spacer</Text>
+<Text type="h3">IPAddressInput</Text>
 <Spacer h={10} />
-<Text>A layout component to space your content.</Text>
+<Text>An input component to display and retrieve IPV4 addresses.</Text>
 <Spacer h={20} />
-<Snippet width="450px" type="lite" text={`import { Spacer } from 'geist-ui-svelte';`} />
+<Snippet width="450px" type="lite" text={`import { IPAddressInput } from 'geist-ui-svelte';`} />
 <Spacer h={30} />
-<Text type="h4">Vertical Spacing</Text>
+<Text type="h4">Basic</Text>
 <Spacer h={10} />
 <FieldSet>
 	<div class="flex flex-col justify-start">
-		<IpAddressInput/>
+		<IPAddressInput value="172 16 10 100"/>
 	</div>
 	<div slot="footer">
 		<Details label="Code">
 			<Code
 				lang="svelte"
-				code={`<Card></Card>
-<Spacer h={30}/>
-<Card></Card>
-<Spacer h={10}/>
-<Card></Card>`}
+				code={`<IpAddressInput value="172 16 10 100"/>`}
+			/>
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">No Dot</Text>
+<Spacer h={5}/>
+<Text>Remove the dot between octets.</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start">
+		<IPAddressInput value="172 16 10 100" noDot/>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<IpAddressInput value="172 16 10 100"/>`}
 			/>
 		</Details>
 	</div>
