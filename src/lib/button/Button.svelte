a60008ffc03519a8557e778219692612fa9ae537
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-27 13:24:09 -0600

    formatting

--- src/lib/button/Button.svelte
+++ src/lib/button/Button.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Loading from "$lib/loading/Loading.svelte";
-	import type { ButtonColor, Size } from "$lib/types.js";
 	import type { HTMLButtonAttributes } from "svelte/elements";
 	import { cva, type VariantProps } from "class-variance-authority";
 	import { cn } from "$lib/util/utils.js";
@@ -24,16 +23,21 @@
 					error: `bg-red-500 text-gray-0 data-[a]:hover:text-red-500 enabled:hover:text-red-500 border-red-500 data-[a]:hover:bg-gray-0 
 					enabled:hover:bg-gray-0 data-[a]:hover:dark:bg-gray-999 enabled:hover:dark:bg-gray-999 dark:border-red-600 dark:bg-red-600`,
 					abort: `bg-transparent dark:bg-transparent border-transparent text-gray-400 dark:text-gray-600`,
+
 					"abort-active": `bg-transparent dark:bg-transparent border-transparent data-[a]:hover:dark:text-gray-0 enabled:hover:dark:text-gray-0
 				  text-gray-400 dark:text-gray-600 data-[a]:hover:text-gray-999 enabled:hover:text-gray-999`,
+
 					"secondary-light": `border-gray-999 bg-gray-999 text-gray-0 data-[a]:hover:bg-gray-900 enabled:hover:bg-gray-900
 					 dark:bg-gray-0 dark:border-gray-0 dark:text-gray-999 data-[a]:dark:bg-gray-100 enabled:hover:dark:bg-gray-100 `,
+
 					"success-light": `bg-blue-600 dark:bg-blue-600 text-gray-0 dark:text-gray-0 dark:border-blue-600 border-blue-600 
 					data-[a]:hover:bg-blue-500 enabled:hover:bg-blue-500 data-[a]:hover:border-blue-500 enabled:hover:border-blue-500 
 					data-[a]:hover:dark:border-blue-500 enabled:hover:dark:border-blue-500 data-[a]:hover:dark:bg-blue-500 enabled:hover:dark:bg-blue-500`,
+
 					"warning-light": `bg-orange-300 text-gray-0 data-[a]:hover:bg-orange-200 enabled:hover:bg-orange-200 border-orange-300 
 					data-[a]:hover:border-orange-200 enabled:hover:border-orange-200 dark:bg-orange-400 dark:text-gray-0 data-[a]:hover:dark:bg-orange-300 
 					enabled:hover:dark:bg-orange-300 dark:border-orange-400 data-[a]:hover:dark:border-orange-300 enabled:hover:dark:border-orange-300`,
+					
 					"error-light": `bg-red-500 text-gray-0 border-red-500 data-[a]:hover:bg-red-400 enabled:hover:bg-red-400 data-[a]:hover:border-red-400 
 					enabled:hover:border-red-400 data-[a]:hover:dark:bg-red-500 enabled:hover:dark:bg-red-500 data-[a]:hover:dark:border-red-500 enabled:hover:dark:border-red-500
 					dark:border-red-600 dark:bg-red-600`,
