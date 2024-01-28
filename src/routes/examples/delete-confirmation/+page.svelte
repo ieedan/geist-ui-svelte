b6a7a820d593a4b45172f53900bd695f9707f1f9
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-28 15:40:50 -0600

    Changed example code

--- src/routes/examples/delete-confirmation/+page.svelte
+++ src/routes/examples/delete-confirmation/+page.svelte
@@ -96,22 +96,23 @@
 	<div class="max-w-[880px] flex justify-center">
 		<Code
 			lang="svelte"
-			code={`<Hero exclusionHeight={79}>
-    <FieldSet>
+			code={`<Hero exclusionHeight={79} class="px-4">
+    <FieldSet color="error">
         <div class="p-2">
             <Text type="h6">Delete Project</Text>
-            <Text type="small">
-                The project will be permanently deleted, including its deployments and domains. 
-                This action is irreversible and can not be undone.
-                </Text>
-            <Spacer h={10}/>
-            <Divider/>
-            <Spacer h={15}/>
+            <Text type="small"
+                >The project will be permanently deleted, including its deployments and domains.
+                This action is irreversible and can not be undone.</Text
+            >
+            <Spacer h={10} />
+            <Divider />
+            <Spacer h={15} />
             <div class="flex place-items-center gap-6">
-                <img 
-                  src={dummyProject} 
-                  alt="your project" 
-                  class="rounded-md border border-gray-100 dark:border-gray-900 overflow-hidden">
+                <img
+                    src={dummyProject}
+                    alt="your project"
+                    class="rounded-md border border-gray-100 dark:border-gray-900 overflow-hidden"
+                />
                 <div class="flex flex-col">
                     <Text type="small" b>echo-chamber</Text>
                     <Text type="small">Last updated 12d ago</Text>
@@ -119,46 +120,43 @@
             </div>
         </div>
         <div slot="footer" class="flex place-items-center justify-end">
-            <Button color="error-light" size="sm" on:click={() => open = true}>Delete</Button>
+            <Button color="error-light" size="sm" on:click={() => (open = true)}>Delete</Button>
         </div>
     </FieldSet>
     <div class="absolute bottom-0 py-2 hidden md:flex place-items-center gap-[2px]">
         <Text type="small" color="abort"
-            >See the code below <ChevronIcon size={16} rotation="90deg" /></Text>
+            >See the code below <ChevronIcon size={16} rotation="90deg" /></Text
+        >
     </div>
 </Hero>
 
-<Modal bind:visible={open} class="w-[500px] h-fit">
+<Modal bind:visible={open} class="sm:w-[500px] h-fit">
     <form on:submit|preventDefault={del} class="p-6 flex flex-col">
         <Text type="h4">Delete Project</Text>
-        <Spacer h={15}/>
+        <Spacer h={15} />
         <Text>
-            This project will be deleted, along with all of its Deployments, 
-            Domains, Environment Variables, Serverless Functions, and Settings.
+            This project will be deleted, along with all of its Deployments, Domains, Environment
+            Variables, Serverless Functions, and Settings.
         </Text>
-        <Spacer h={20}/>
+        <Spacer h={20} />
         <Note color="error">This action is not reversible. Please be certain.</Note>
-        <Spacer h={20}/>
-        <Divider/>
-        <Spacer h={15}/>
+        <Spacer h={20} />
+        <Divider />
+        <Spacer h={15} />
         <Input width="100%">
             Enter the project name <Text type="small" b>ieedan/echo-chamber</Text> to continue:
         </Input>
-        <Spacer h={15}/>
-        <Spacer h={5}/>
+        <Spacer h={15} />
+        <Spacer h={5} />
         <Input width="100%">
             To verify, type <Text type="small" b>delete my project</Text> below:
         </Input>
-        <Spacer h={15}/>
-        <Divider/>
-        <Spacer h={15}/>
+        <Spacer h={15} />
+        <Divider />
+        <Spacer h={15} />
         <div class="flex place-items-center justify-between">
-            <Button on:click={() => open = true}>
-                Cancel
-            </Button>
-            <Button type="submit" color="secondary-light" loading={deleting}>
-                Continue
-            </Button>
+            <Button on:click={() => (open = true)}>Cancel</Button>
+            <Button type="submit" color="secondary-light" loading={deleting}>Continue</Button>
         </div>
     </form>
 </Modal>`}
