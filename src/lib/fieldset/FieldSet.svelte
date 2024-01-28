d0a8876c99cd749ade47b34b32acdaacba5de891
Aidan Bleser <aidanbleser35@gmail.com>
2024-01-28 15:28:46 -0600

    Added fieldset colors

--- src/lib/fieldset/FieldSet.svelte
+++ src/lib/fieldset/FieldSet.svelte
@@ -1,9 +1,30 @@
-<div class="border border-gray-100 rounded-lg overflow-hidden dark:border-gray-900">
-	<div class="px-4 py-3">
+<script lang="ts">
+	import { cn } from "$lib/util/utils.js";
+	import { cva, type VariantProps } from "class-variance-authority";
+
+	const style = cva("border rounded-lg overflow-hidden",{
+		variants: {
+			color: {
+				default: "bg-gray-50 dark:bg-gray-950 border-gray-100 dark:border-gray-900",
+				success: "border-blue-600 bg-blue-600 bg-opacity-50 dark:border-blue-600 dark:bg-blue-600 dark:bg-opacity-35",
+				warning: "border-orange-300 bg-orange-300 bg-opacity-50 dark:border-orange-400 dark:bg-orange-400 dark:bg-opacity-35",
+				error: "border-red-600 bg-red-600 bg-opacity-50 dark:border-red-600 dark:bg-red-600 dark:bg-opacity-35",
+				transparent: "border-gray-100 dark:border-gray-900",
+			}
+		}
+	})
+
+	interface Props extends VariantProps<typeof style>{};
+
+	export let color: Props["color"] = "default";
+</script>
+
+<div class={cn(style({color}))}>
+	<div class="px-4 py-3 bg-gray-0 dark:bg-gray-999">
 		<slot />
 	</div>
 	<div
-		class="px-4 py-2 bg-gray-50 border-t border-gray-100 dark:border-gray-900 dark:bg-gray-950"
+		class="px-4 py-2 border-t border-inherit dark:border-inherit bg-transparent"
 	>
 		<slot name="footer" />
 	</div>
--- src/routes/components/fieldset/+page.svelte
+++ src/routes/components/fieldset/+page.svelte
@@ -4,6 +4,7 @@
 	import Details from "$lib/details/Details.svelte";
 	import FieldSet from "$lib/fieldset/FieldSet.svelte";
 	import Input from "$lib/input/Input.svelte";
+	import Skeleton from "$lib/skeleton/Skeleton.svelte";
 	import Snippet from "$lib/snippet/Snippet.svelte";
 	import Spacer from "$lib/spacer/Spacer.svelte";
 	import Text from "$lib/text/Text.svelte";
@@ -31,7 +32,7 @@
 				<Input value="Team Name" />
 			</div>
 			<div slot="footer" class="flex justify-between place-items-center">
-				<Text type="small">Please use 32 characters at maximum.</Text>
+				<Text type="small" color="secondary">Please use 32 characters at maximum.</Text>
 				<Button color="secondary-light" size="sm">Save</Button>
 			</div>
 		</FieldSet>
@@ -52,8 +53,102 @@
 		<Input value="Team Name" />
 	</div>
 	<div slot="footer" class="flex justify-between place-items-center">
-		<Text type="small">Please use 32 characters at maximum.</Text>
+		<Text type="small" color="secondary">Please use 32 characters at maximum.</Text>
 		<Button color="secondary-light" size="sm">Save</Button>
+	</div>
+</FieldSet>`}
+			/>
+		</Details>
+	</div>
+</FieldSet>
+<Spacer h={30} />
+<Text type="h4">Colors</Text>
+<Spacer h={10} />
+<FieldSet>
+	<div class="flex flex-col justify-start gap-3">
+		<FieldSet>
+			<div class="py-2">
+				<Spacer h={10}/>
+			</div>
+			<div slot="footer" class="flex place-items-center py-1">
+				<Spacer h={10}/>
+			</div>
+		</FieldSet>
+		<FieldSet color="success">
+			<div class="py-2">
+				<Spacer h={10}/>
+			</div>
+			<div slot="footer" class="flex place-items-center py-1">
+				<Spacer h={10}/>
+			</div>
+		</FieldSet>
+		<FieldSet color="warning">
+			<div class="py-2">
+				<Spacer h={10}/>
+			</div>
+			<div slot="footer" class="flex place-items-center py-1">
+				<Spacer h={10}/>
+			</div>
+		</FieldSet>
+		<FieldSet color="error">
+			<div class="py-2">
+				<Spacer h={10}/>
+			</div>
+			<div slot="footer" class="flex place-items-center py-1">
+				<Spacer h={10}/>
+			</div>
+		</FieldSet>
+		<FieldSet color="transparent">
+			<div class="py-2">
+				<Spacer h={10}/>
+			</div>
+			<div slot="footer" class="flex place-items-center py-1">
+				<Spacer h={10}/>
+			</div>
+		</FieldSet>
+	</div>
+	<div slot="footer">
+		<Details label="Code">
+			<Code
+				lang="svelte"
+				code={`<FieldSet>
+	<div class="py-2">
+		<Spacer h={10}/>
+	</div>
+	<div slot="footer" class="flex place-items-center py-1">
+		<Spacer h={10}/>
+	</div>
+</FieldSet>
+<FieldSet color="success">
+	<div class="py-2">
+		<Spacer h={10}/>
+	</div>
+	<div slot="footer" class="flex place-items-center py-1">
+		<Spacer h={10}/>
+	</div>
+</FieldSet>
+<FieldSet color="warning">
+	<div class="py-2">
+		<Spacer h={10}/>
+	</div>
+	<div slot="footer" class="flex place-items-center py-1">
+		<Spacer h={10}/>
+	</div>
+</FieldSet>
+<FieldSet color="error">
+	<div class="py-2">
+		<Spacer h={10}/>
+	</div>
+	<div slot="footer" class="flex place-items-center py-1">
+		<Spacer h={10}/>
+	</div>
+</FieldSet>
+<FieldSet color="secondary">
+	<div class="py-2">
+		<Spacer h={10}/>
+	</div>
+	<div slot="footer" class="flex place-items-center py-1">
+		<Spacer h={10}/>
 	</div>
 </FieldSet>`}
 			/>
