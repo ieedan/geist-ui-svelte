127bc637e48bd120cebf4184cffa7e5a6d2c53c6
Aidan Bleser <aidan.bleser@adient.com>
2024-01-31 15:29:14 -0600

    Update TextArea.svelte
    Fixes #190

--- src/lib/textarea/TextArea.svelte
+++ src/lib/textarea/TextArea.svelte
@@ -59,7 +59,9 @@
 		data-size={size}
 		class="resize-none bg-gray-0 dark:bg-gray-999 border border-gray-100
 		px-2 py-1 disabled:dark:bg-gray-950 disabled:bg-gray-50
-		placeholder:text-gray-300 text-gray-999 dark:placeholder:text-gray-600
+		disabled:text-gray-300 disabled:dark:text-gray-600
+		disabled:placeholder:text-gray-300 disabled:dark:placeholder:text-gray-600
+		text-gray-999 placeholder:text-gray-200 dark:placeholder:text-gray-600
 		dark:text-gray-0 disabled:hover:cursor-not-allowed
 		dark:border-gray-900 outline-none focus:border-gray-200
 		dark:focus:border-gray-800 transition-all rounded-md w-fit min-w-0 data-[size='sm']:text-sm
