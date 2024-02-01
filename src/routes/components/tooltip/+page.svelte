77a6f26f6e6a3bc19e8972570cf7ceffd882a4b5
Aidan Bleser <aidanbleser35@gmail.com>
2024-02-01 17:14:08 -0600

    Tooltip Rewrite

--- src/routes/components/tooltip/+page.svelte
+++ src/routes/components/tooltip/+page.svelte
@@ -49,35 +49,35 @@
 <Spacer h={30} />
 <Note color="warning">This component is experimental</Note>
 <Spacer h={20} />
-<Text type="h4">Vertical Spacing</Text>
+<Text type="h4">Basic</Text>
 <Spacer h={10} />
 <FieldSet>
 	<div class="flex gap-2 justify-start">
 		<Button id="button-id">Hover me</Button>
-		<ToolTip anchor="#button-id">Hovered</ToolTip>
+		<ToolTip anchor="#button-id" content='Hovered' config={{ animateFill: false }}/>
 	</div>
 	<div slot="footer">
 		<Details label="Code">
 			<Code
 				lang="svelte"
 				code={`<Button id="button-id">Hover me</Button>
-<ToolTip anchor="#button-id">
-    Hovered
-</ToolTip>`}
+<ToolTip anchor="#button-id" content='Hovered'/>`}
 			/>
 		</Details>
 	</div>
 </FieldSet>
 <Spacer h={30} />
-<Text type="h4">Placements</Text>
+<Note color="success">
+	The Tooltip component uses tippy under the hood 
+</Note>
+<Spacer h={20} />
+<Text type="h4">Placement</Text>
 <Spacer h={10} />
 <FieldSet>
 	<div class="flex flex-wrap gap-2 justify-start">
 		{#each placements as placement}
 			<Button id="placement-{placement}">{placement}</Button>
-			<ToolTip anchor="#placement-{placement}" {placement}>
-				{placement} Hover
-			</ToolTip>
+			<ToolTip anchor="#placement-{placement}" {placement} content="{placement} Hover" />
 		{/each}
 	</div>
 	<div slot="footer">
@@ -87,90 +87,3 @@
 	</div>
 </FieldSet>
 <Spacer h={30} />
-<Note color="success"
-	>The Tooltip component uses the Dropdown component under the hood so it inherits many of its
-	features.</Note
->
-<Spacer h={20} />
-<Text type="h4">Animation</Text>
-<Spacer h={5} />
-<Text>
-	By default the animation is disabled but you can turn it on using the <code>`animate`</code> attribute.
-</Text>
-<Spacer h={10} />
-<FieldSet>
-	<div class="flex flex-wrap gap-2 justify-start">
-		<Button id="default-id">Hover me</Button>
-		<ToolTip anchor="#default-id">Hovered</ToolTip>
-		<Button id="animate-id">Hover me</Button>
-		<ToolTip anchor="#animate-id" animate>Hovered</ToolTip>
-	</div>
-	<div slot="footer">
-		<Details label="Code">
-			<Code
-				lang="svelte"
-				edits={[{ number: 9, type: "add" }]}
-				code={`<Button id="default-id">Hover me</Button>
-<ToolTip 
-  anchor="#default-id">
-    Hovered
-</ToolTip>
-<Button id="animate-id">Hover me</Button>
-<ToolTip 
-  anchor="#animate-id" 
-  animate>
-  Hovered
-</ToolTip>`}
-			/>
-		</Details>
-	</div>
-</FieldSet>
-<Spacer h={30} />
-<Text type="h4">Shadow</Text>
-<Spacer h={10} />
-<FieldSet>
-	<div class="flex flex-wrap gap-2 justify-start">
-		<Button id="shadow-id">Hover me</Button>
-		<ToolTip anchor="#shadow-id" shadow>Hovered</ToolTip>
-	</div>
-	<div slot="footer">
-		<Details label="Code">
-			<Code
-				lang="svelte"
-				edits={[{ number: 4, type: "add" }]}
-				code={`<Button id="shadow-id">Hover me</Button>
-<ToolTip 
-  anchor="#shadow-id" 
-  shadow>
-  Hovered
-</ToolTip>`}
-			/>
-		</Details>
-	</div>
-</FieldSet>
-<Spacer h={30} />
-<Text type="h4">Event</Text>
-<Spacer h={10} />
-<FieldSet>
-	<div class="flex flex-wrap gap-2 justify-start">
-		<Button id="hover-id">Hover me</Button>
-		<ToolTip anchor="#hover-id" shadow>Hovered</ToolTip>
-		<Button id="click-id">Click Me</Button>
-		<ToolTip anchor="#click-id" shadow event="click/click">Clicked</ToolTip>
-		<Button id="focus-id">Focus Me</Button>
-		<ToolTip anchor="#focus-id" shadow event="focus/blur">Focused</ToolTip>
-	</div>
-	<div slot="footer">
-		<Details label="Code">
-			<Code
-				lang="svelte"
-				code={`<Button id="hover-id">Hover me</Button>
-<ToolTip anchor="#hover-id" shadow>Hovered</ToolTip>
-<Button id="click-id">Click Me</Button>
-<ToolTip anchor="#click-id" shadow event="click/click">Clicked</ToolTip>
-<Button id="click-id">Focus Me</Button>
-<ToolTip anchor="#click-id" shadow event="focus/blur">Focused</ToolTip>`}
-			/>
-		</Details>
-	</div>
-</FieldSet>
